ca39412c6f9bca9ef4fc35f692c1acd96007b9e8
Meron Brouwer <meron.brouwer@gmail.com>
2019-02-14 09:16:39 +0100

    first commit

---
+++ src/main/java/nl/han/ica/oose/dea/meron/valentijn/App.java
@@ -0,0 +1,13 @@
+package nl.han.ica.oose.dea.meron.valentijn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
